b4fe933840356ac7a407aaaf5127134e0cf70b55
vinodyadav9 <vinod.yadav@slovink.com>
2025-10-29 11:18:02 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@
 ## Labels module callled that will be used for naming and tags.
 ##----------------------------------------------------------------------------------
 module "labels" {
-  source      = "git::https://github.com/OpsStation/terraform-multicloud-labels.git?ref=v1.0.0"
+  source      = "opsstation/labels/multicloud"
+  version     = "1.0.0"
   name        = "payment-api"
   environment = "prod"
   repository  = "terraform-multicloud-labels"
